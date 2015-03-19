e68ac116cf40c3cc79a8c07c9c738aa35dfb12f9
Faruk Sahin <fsahin@spotify.com>
2015-03-19 23:04:34 +0100

    Implement simple rate limiting

--- server/server.py
+++ server/server.py
@@ -5,6 +5,9 @@ from werkzeug.contrib.cache import SimpleCache
 import pyen
 import redis
 import uuid
+import time
+from functools import update_wrapper
+from flask import request, g
 
 cache = SimpleCache(threshold=20000)
 
@@ -23,6 +26,44 @@ cors = CORS(app)
 en = pyen.Pyen()
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+class RateLimit(object):
+    expiration_window = 10
+
+    def __init__(self, key_prefix, limit, per, send_x_headers):
+        self.reset = (int(time.time()) // per) * per + per
+        self.key = key_prefix + str(self.reset)
+        self.limit = limit
+        self.per = per
+        self.send_x_headers = send_x_headers
+        p = r.pipeline()
+        p.incr(self.key)
+        p.expireat(self.key, self.reset + self.expiration_window)
+        self.current = min(p.execute()[0], limit)
+
+    remaining = property(lambda x: x.limit - x.current)
+    over_limit = property(lambda x: x.current >= x.limit)
+
+def get_view_rate_limit():
+    return getattr(g, '_view_rate_limit', None)
+
+def on_over_limit(limit):
+    return 'You hit the rate limit', 400
+
+def ratelimit(limit, per=300, send_x_headers=True,
+              over_limit=on_over_limit,
+              scope_func=lambda: request.remote_addr,
+              key_func=lambda: request.endpoint):
+    def decorator(f):
+        def rate_limited(*args, **kwargs):
+            key = 'rate-limit/%s/%s/' % (key_func(), scope_func())
+            rlimit = RateLimit(key, limit, per, send_x_headers)
+            g._view_rate_limit = rlimit
+            if over_limit is not None and rlimit.over_limit:
+                return over_limit(rlimit)
+            return f(*args, **kwargs)
+        return update_wrapper(rate_limited, f)
+    return decorator
 
 def cached(timeout=5 * 60, key='view/%s'):
     def decorator(f):
@@ -68,6 +109,7 @@ def get_all_genres():
 
 
 @app.route('/api/savetree', methods=['POST'])
+@ratelimit(limit=3, per=10) # 3 times in 10 secs
 def save_entry():
     entry_id = str(uuid.uuid4()).replace('-', '')
     entry_data = request.form['entry_data']
